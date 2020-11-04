2f4316afffd5a7f4b575eab03f06e9df4c5bad0d
Franklin-Molina <asdfglxl7@gmail.com>
2020-11-03 23:24:59 -0500

    backend-auth-me

--- api/productos/controllers/productos.js
+++ api/productos/controllers/productos.js
@@ -1,8 +1,46 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+
+module.exports = {
+    me: async(ctx) => {
+        //autenticacion del usuario
+        const user = ctx.state.user
+
+
+        let productos = await strapi.services.productos.find({
+            user: user.id
+        })
+        ctx.send(productos)
+        return productos
+
+    },
+    async create(ctx) {
+
+        let entity;
+        const user = ctx.state.user;
+
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            data.user = user.id
+            entity = await strapi.services.productos.create(data, { files })
+        } else {
+            //Formato json
+            const data = ctx.request.body
+
+            data.user = user.id;
+            entity = await strapi.services.productos.create(data);
+
+        }
+
+        //parcea la info luego la arma 
+        return sanitizeEntity(entity, { model: strapi.models.productos });
+    },
+
+};
